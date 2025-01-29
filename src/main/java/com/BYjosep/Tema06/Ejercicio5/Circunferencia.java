9a22312185f779c293466ac6448a5d5ee56094b5
Josep Vicent Mestre Llobell <jmestrellobell@gmail.com>
2025-01-29 16:13:58 +0100

    Update

--- src/main/java/com/BYjosep/Tema06/Ejercicio5/Circunferencia.java
+++ src/main/java/com/BYjosep/Tema06/Ejercicio5/Circunferencia.java
@@ -4,7 +4,46 @@ import com.BYjosep.Tema06.Ejercicio4.Punto;
 
 public class Circunferencia {
     Punto punto1 = new Punto();
-    double circunferencia;
+    double radio;
+
+    /*
+     *****************
+     * Constructores *
+     *****************
+    */
+
+    Circunferencia() {
+        radio = 0;
+        punto1.setX(0);
+        punto1.setY(0);
+    }
+
+    Circunferencia(double radio, Punto punto1) {
+        this.radio = radio;
+        this.punto1 = punto1;
+    }
+
+    Circunferencia(double radio, double puntoX, double puntoY) {
+    this.radio =radio;
+    punto1.setX(puntoX);
+    punto1.setY(puntoY);
+    }
 
 
+
+    public double getRadio() {
+        return radio;
+    }
+
+    public void setRadio(double radio) {
+        this.radio = radio;
+    }
+
+    @Override
+    public String toString() {
+
+        return "Circunferencia{" +
+                "circunferencia=" + radio +
+                '}';
+    }
 }
