809eb57ffa98c557a719e3be66944b382a3c2c2e
Josep Vicent Mestre Llobell <59211313+BYjosep@users.noreply.github.com>
2025-02-25 01:22:17 +0100

    Debería de funcionar este constructor de coche mejorado de forma correcta

--- src/main/java/com/BYjosep/Tema06/CocheMejorado/Coche.java
+++ src/main/java/com/BYjosep/Tema06/CocheMejorado/Coche.java
@@ -2,55 +2,51 @@ package com.BYjosep.Tema06.CocheMejorado;
 
 public class Coche {
     private final String ERROR = "No se pueden añadir más matrículas";
-    private String matricula;
+    private final String matricula;
     private int numeroMatricula = 0;
-    private char[] letrasMatricula = {'A','A','A'};
-    private String marca;
-    private String modelo;
-    private String color;
-    private boolean pinturaMetalizada;
+    private final char[] letrasMatricula = {'A', 'A', 'A'};
+    private final String marca;
+    private final String modelo;
+    private final String color;
+    private final boolean pinturaMetalizada;
 
-public Coche(String marca, String modelo, String color, boolean pinturaMetalizada) {
-    this.matricula = setMatricula();
-    this.marca = marca;
-    this.modelo = modelo;
-    this.color = color;
-    this.pinturaMetalizada = pinturaMetalizada;
-}
-
+    public Coche(String marca, String modelo, String color, boolean pinturaMetalizada) {
+        this.matricula = setMatricula();
+        this.marca = marca;
+        this.modelo = modelo;
+        this.color = color;
+        this.pinturaMetalizada = pinturaMetalizada;
+    }
 
 
     private String setMatricula() {
-        if (letrasMatricula[0] >= 'Z' && numeroMatricula >= 9999){
+        if (letrasMatricula[0] >= 'Z' && numeroMatricula >= 9999) {
             return ERROR;
         }
 
-        StringBuilder sb = new StringBuilder();
-        sb.append(String.format("%40D", numeroMatricula));
-        sb.append(letrasMatricula);
+        String sb = String.format("%40D", numeroMatricula) +
+                String.valueOf(letrasMatricula);
 
-        if(numeroMatricula == 9999) {
-            numeroMatricula=0;
-            if(letrasMatricula[2] == 'Z') {
+        if (numeroMatricula == 9999) {
+            numeroMatricula = 0;
+            if (letrasMatricula[2] == 'Z') {
 
                 if (letrasMatricula[1] == 'Z') {
                     letrasMatricula[0] = (char) (letrasMatricula[0] + 1);
                     letrasMatricula[1] = 'A';
 
-                }
-                else {
+                } else {
                     letrasMatricula[1] = (char) (letrasMatricula[1] + 1);
                 }
 
                 letrasMatricula[2] = 'A';
-            }
-            else {
+            } else {
                 letrasMatricula[2] = (char) (letrasMatricula[2] + 1);
             }
-        }else {
+        } else {
             numeroMatricula++;
         }
 
-        return sb.toString();
+        return sb;
     }
 }
