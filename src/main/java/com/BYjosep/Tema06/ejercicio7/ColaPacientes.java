96defde7b670e2edef10be4f31549c5c0287350b
Josep Vicent Mestre Llobell <jmestrellobell@gmail.com>
2025-02-27 17:08:35 +0100

    update aun falta codigo

--- src/main/java/com/BYjosep/Tema06/ejercicio7/ColaPacientes.java
+++ src/main/java/com/BYjosep/Tema06/ejercicio7/ColaPacientes.java
@@ -1,8 +1,81 @@
 package com.BYjosep.Tema06.ejercicio7;
 
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class ColaPacientes {
 
+    private final int TAMANO_MAXIMO;
     Pacientes[] pacientes;
+    private int size;
+
+    ColaPacientes(int TAMANO_MAXIMO) {
+        this.TAMANO_MAXIMO = TAMANO_MAXIMO;
+        pacientes = new Pacientes[TAMANO_MAXIMO];
+        this.size = 0;
+    }
+
+
+    public boolean nuevoPaciente(int sip, String nombre, char sexo, int edad,
+                                 LocalDate fechaEntrada, LocalTime horaEntrada,
+                                 String sintoma) {
+        if (size == TAMANO_MAXIMO) {
+            return false;
+        }
+        pacientes[size] = new Pacientes(sip, nombre, sexo, edad,
+                fechaEntrada, horaEntrada, sintoma);
+        size++;
+        return true;
+    }
+
+    public boolean atenderPaciente(int sip, float temperatura, int ppm, float tensionSistolica,
+                                   float tensionDiastolica) {
+        if (size == 0) {
+            return false;
+        }
+        for (int i = size--; i > 0; i--) {
+            if (sip == pacientes[i].getSip() && pacientes[i].getFechaAlta() == null) {
+                System.out.println(pacientes[i]);
+                pacientes[i].setConstantes(temperatura, ppm, tensionSistolica, tensionDiastolica);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean altaMedica(int sip, String motivoDelAlta) {
+        if (size == 0) return false;
+        for (int i = size - 1; i > 0; i--) {
+            if (sip == pacientes[i].getSip() && !pacientes[i].getAltaMedica()) {
+                pacientes[i].setMotivoAlta(motivoDelAlta);
+                pacientes[i].setFechaAlta(LocalDate.now());
+                pacientes[i].setHoraAlta(LocalTime.now());
+                pacientes[i].setAltaMedica();
+                return true;
+            }
+        }
+        return false;
+    }
+
+        /*
+
+    public Pacientes[] consultaPorSip(int sip) {
+        if (size == 0) return null;
+
+    }
+
+    public Pacientes[] consultaPorFechas(LocalDate fecha) {
+        if (size == 0) return null;
+
+    }
+
+    public Pacientes[] consultaHistorico(){
+        if (size == 0) return null;
+
+    }
+
+
+         */
 
 }
--- src/main/java/com/BYjosep/Tema06/ejercicio7/Pacientes.java
+++ src/main/java/com/BYjosep/Tema06/ejercicio7/Pacientes.java
@@ -4,6 +4,10 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 
 public class Pacientes {
+    public enum constantes {
+        TEMPERATURA, PPM, TENSION_SISTOLICA, TENSIOM_DIASTOLICA
+    }
+
     private final int sip;
     private final String nombre;
     private final char sexo;
@@ -15,6 +19,8 @@ public class Pacientes {
     private LocalDate fechaAlta;
     private LocalTime horaAlta;
     private String motivoAlta;
+    private boolean altaMedica;
+
     public Pacientes(int sip, String nombre, char sexo, int edad,
                      LocalDate fechaEntrada, LocalTime horaEntrada,
                      String sintoma) {
@@ -26,30 +32,75 @@ public class Pacientes {
         this.horaEntrada = horaEntrada;
         this.sintoma = sintoma;
         this.preRev = new float[4];
-        this.fechaAlta = null;
-        this.horaAlta = null;
-        this.motivoAlta = null;
+        this.altaMedica = false;
+
     }
 
-    public boolean setTemperatura(float temperatura) {
+    /**
+     * setter para hacer la medicion con las constantes vitales
+     *
+     * @param temperatura       se usa un float
+     * @param ppm               se usa un int
+     * @param tensionSistolica  se usa un float
+     * @param tensionDiastolica se usa un float
+     * @return en caso de no poder añadir un valor devuelve false
+     */
+    public boolean setConstantes(float temperatura, int ppm, float tensionSistolica, float tensionDiastolica) {
+        boolean check;
+        check = setTemperatura(temperatura);
+        if (!check) return false;
+        check = setPpm(ppm);
+        if (!check) return false;
+        check = setTensionSistolica(tensionSistolica);
+        if (!check) return false;
+
+        check = setTensionDiastolica(tensionDiastolica);
+
+        return check;
+
+    }
+
+    public void setFechaAlta(LocalDate fechaAlta) {
+        this.fechaAlta = fechaAlta;
+    }
+
+    public void setHoraAlta(LocalTime horaAlta) {
+        this.horaAlta = horaAlta;
+    }
+
+    public void setAltaMedica() {
+        this.altaMedica = true;
+    }
+
+    public void setMotivoAlta(String motivoAlta) {
+        this.motivoAlta = motivoAlta;
+    }
+
+    private boolean setTemperatura(float temperatura) {
+        if (preRev[0] == 0 && temperatura == 0) return false;
         preRev[0] = temperatura;
         return true;
     }
 
-    public boolean setPpm(int ppm) {
+    private boolean setPpm(int ppm) {
+        if (preRev[1] == 0 && ppm == 0)return false;
         preRev[1] = ppm;
         return true;
     }
 
-    public boolean setTensionSistolica(float tensionsiStolica) {
-        preRev[2] = tensionsiStolica;
+    private boolean setTensionSistolica(float tensionSistolica) {
+        if (preRev[2] == 0 && tensionSistolica == 0) return false;
+        preRev[2] = tensionSistolica;
         return true;
     }
 
-    public boolean setTensionDiastolica(float tensionDiastolica) {
+    private boolean setTensionDiastolica(float tensionDiastolica) {
+        if (preRev[3] == 0 && tensionDiastolica == 0)return false;
         preRev[3] = tensionDiastolica;
         return true;
     }
+
+
 
     @Override
     public String toString() {
@@ -62,18 +113,66 @@ public class Pacientes {
         sb.append(", fechaEntrada=").append(fechaEntrada);
         sb.append(", horaEntrada=").append(horaEntrada);
         sb.append(", sintoma=").append(sintoma);
-        sb.append(", preRev=");
+        if (preRev != null) {
+            sb.append(", preRev=");
 
-        for (int i = 0; i < preRev.length; i++) {
-            sb.append(constantes.valueOf(String.valueOf(i))).append("=").append(preRev[i]);
+            for (int i = 0; i < preRev.length; i++) {
+                sb.append(constantes.valueOf(String.valueOf(i))).append("=").append(preRev[i]);
+            }
         }
+
 
         sb.append('}');
 
         return sb.toString();
     }
 
-    public enum constantes {
-        TEMPERATURA, PPM, TENSION_SISTOLICA, TENSIOM_DIASTOLICA
+
+    public int getSip() {
+        return sip;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public LocalDate getFechaEntrada() {
+        return fechaEntrada;
+    }
+
+    public LocalTime getHoraEntrada() {
+        return horaEntrada;
+    }
+
+    public String getSintoma() {
+        return sintoma;
+    }
+
+    public float[] getPreRev() {
+        return preRev;
+    }
+
+    public LocalDate getFechaAlta() {
+        return fechaAlta;
+    }
+
+    public LocalTime getHoraAlta() {
+        return horaAlta;
+    }
+
+    public String getMotivoAlta() {
+        return motivoAlta;
+    }
+
+    public boolean getAltaMedica() {
+        return altaMedica;
     }
 }
